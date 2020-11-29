5ed21a3854e76b8dd01a0a2b64d7c0bc74a16a27
callmejojo <jingwangjolie@gmail.com>
2020-11-29 23:01:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Fizz Buzz Tryout
+for(var i=0; i<101; i++){
+  if (i % 15 === 0) console.log("FizzBuzz");
+  else if (i % 5 === 0) console.log("Buzz");
+  else if (i % 3 === 0) console.log("Fizz");
+  else console.log(i);
+}
+// QQ1: Why do others use "let" instead of var?
+// QQ2: Is there any other easier solution to this?
