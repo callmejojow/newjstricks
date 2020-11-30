5f5a6a3ab34f7eca159fe361a9a1d7199a4fc2bf
callmejojo <jingwangjolie@gmail.com>
2020-11-30 00:10:23 +0000

    here's more

--- index.js
+++ index.js
@@ -7,3 +7,45 @@ for (var i = 0; i < 101; i++) {
 }
 // QQ1: Why do others use "let" instead of var?
 // QQ2: Is there any other easier solution to this?
+
+function greet() {
+  return "Good Morning!"
+}
+
+// call the function
+var dogname = greet()
+console.log(dogname)
+
+
+// declare the function
+function greeting(name){
+  console.log("Good Morning, " + name + "!");
+}
+
+// call the function with a parameter
+greeting("Maggie");
+
+
+// declare the function
+function divide(numberOne, numberTwo) {
+  console.log(numberOne / numberTwo);
+}
+
+// call the function
+// should return 5
+divide(10, 2)
+
+
+
+// declare the function
+function largestNum(num1, num2) {
+if (num1 > num2){
+  return num1;
+} 
+else if(num1 < num2){
+  return num2;
+} else{
+  return false;
+}
+}
+largestNum(3,8);
